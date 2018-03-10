57e9ae7c60b3b8c043da1cab148fec373c26f968
Alexander McNicholl <alexander.mcnicholl@hotmail.com>
2018-03-10 11:34:48 +1000

    Add files via upload

--- PHPCommandBridge/Scripts/main.js
+++ PHPCommandBridge/Scripts/main.js
@@ -1,8 +1,8 @@
-function executeCommand(command, outDiv, inputs=null) {
+function executeCommand(command, outElement, inputs=null) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById(outDiv).innerHTML = this.responseText;
+			document.getElementById(outElement).innerHTML = this.responseText;
 		}
 	};
 	xhttp.open('POST', 'Processor.php?command=' + command, true);
