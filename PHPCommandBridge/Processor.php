5ff34daf4a87c982adce84f994057ad5e764085f
AlexanderMcNicholl <alexander.mcnicholl@hotmail.com>
2018-03-11 08:39:57 +1000

    Updated and cleaned code, also added comments.

--- PHPCommandBridge/Processor.php
+++ PHPCommandBridge/Processor.php
@@ -1,8 +1,8 @@
 <?php 
 function userInput($input, $procPipes) {
-	fwrite($procPipes[0], $input);
+	fwrite($procPipes[0], $input); // Writes user input.
 	if ($input !== "") {
-		fwrite($procPipes[0], "\n");
+		fwrite($procPipes[0], "\n"); // Writes a new line pipe to the user can have multiple inputs.
 	}
 }
 function closeProcess($procPipes, $process) { //DO NOT FORGET TO CLOSE PROCESS!
@@ -10,25 +10,30 @@ function closeProcess($procPipes, $process) { //DO NOT FORGET TO CLOSE PROCESS!
 	$return_value = proc_close($process);
 	echo "\nprocess executed exit value of $return_value\n";
 }
+// This function ques the process to be executing when ready.
 function queProcess($cmd, $input='') {
 	$descriptorspec = array(
-		0 => array("pipe", "r"),
-		1 => array("pipe", "w"),
-		2 => array("file", "tmp/error_log/error-output.txt", "a")
+		// The first pipes is the input for the I/O stream. (stdin)
+		0 => array("pipe", "r"), 
+		// The second pipe is the output for the I/O stream. (stdout) 
+		1 => array("pipe", "w"), 
+		// The last pipe is the error for the I/O stream. (stderr)
+		2 => array("file", "tmp/error_log/error-output.txt", "a") 
 	);
+	// This will start the process and open an I/O stream.
 	$process = proc_open($cmd, $descriptorspec, $pipes);
-	echo '<pre>';
-	if (is_resource($process)) {
+	echo '<pre>'; // The pre tags keeps the outputs in the original formatting.
+	if (is_resource($process)) { // This checks if it is a resource.
 		//Start user inputs.
-		userInput($input, $pipes);
+		userInput($input, $pipes); // This writes data (fwrite) into pipe[0] (stdin).
 		//End user inputs.
-		fclose($pipes[0]);
+		fclose($pipes[0]); // This closes the input pipes, data can no longer be writting to pipe[0].
 	}
-	echo stream_get_contents($pipes[1]);
-	closeProcess($pipes, $process);	
-	echo '</pre>';
+	echo stream_get_contents($pipes[1]); // This dumps the process's output.
+	closeProcess($pipes, $process);	 // This will finally close the process and all of the I/O pipes.
+	echo '</pre>'; // This ends the pre tag.
 }
-$command = $_REQUEST['command'];
-$inputs = $_REQUEST['input'];
-queProcess($command, $inputs);
+$command = $_REQUEST['command']; // Retrieving data from AJAX call. <= The command to be executed.
+$inputs = $_REQUEST['input']; // <= The user response to command. E.G. a readlines call.
+queProcess($command, $inputs); // This will que to the process to be executed. 
 ?>
--- PHPCommandBridge/Scripts/main.js
+++ PHPCommandBridge/Scripts/main.js
@@ -1,14 +1,24 @@
 function executeCommand(command, outElement, inputs = null, exec_function = null) {
+	if (exec_function != null) {
+		AjaxCall('Processor.php?command=' + command + '&' + 'input=', exec_function(this.responseText));	
+	} else {
+		AjaxCall('Processor.php?command=' + command + '&' + 'input=', function(data) {
+			editElementText(outElement, data);
+		});
+	}
+}
+function AjaxCall(url, exec_function) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4) {
-			if (exec_function != null) {
-				exec_function(this.responseText);
-			} else {
-				document.getElementById(outElement).innerHTML = this.responseText;
-			}
+		if (this.readyState == 4 && this.status == 200) {
+			exec_function(this.responseText);
+		} else {
+			exec_function("Executing function...");
 		}
-	};
-	xhttp.open('POST', 'Processor.php?command=' + command + '&' + 'input=' + inputs, true);
+	}
+	xhttp.open('POST', url, true);
 	xhttp.send();
+}
+function editElementText(element, text) {
+	document.getElementById(element).innerHTML = text;
 }
