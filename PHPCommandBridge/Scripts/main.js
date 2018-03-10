13de82f0ec531ed2728f12e7685e5800a11e56da
AlexanderMcNicholl <alexander.mcnicholl@hotmail.com>
2018-03-10 17:17:41 +1000

    Added new function feature

--- PHPCommandBridge/Scripts/main.js
+++ PHPCommandBridge/Scripts/main.js
@@ -1,8 +1,12 @@
-function executeCommand(command, outElement, inputs = null) {
+function executeCommand(command, outElement, inputs = null, exec_function = null) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if (this.readyState == 4) {
-			document.getElementById(outElement).innerHTML = this.responseText;
+			if (exec_function != null) {
+				exec_function(this.responseText);
+			} else {
+				document.getElementById(outElement).innerHTML = this.responseText;
+			}
 		}
 	};
 	xhttp.open('POST', 'Processor.php?command=' + command + '&' + 'input=' + inputs, true);
