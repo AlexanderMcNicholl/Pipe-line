b8e4ea251a99fce1c963e3c6e0edd8b64ea80e1e
Alexander McNicholl <alexander.mcnicholl@hotmail.com>
2018-03-10 14:38:45 +1000

    Autofocus in test document

--- PHPCommandBridge/Scripts/main.js
+++ PHPCommandBridge/Scripts/main.js
@@ -1,7 +1,7 @@
-function executeCommand(command, outElement, inputs=null) {
+function executeCommand(command, outElement, inputs = null) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState == 4) {
 			document.getElementById(outElement).innerHTML = this.responseText;
 		}
 	};
