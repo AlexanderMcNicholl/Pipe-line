a5aabd5f902328ff9b5792e3fc166f7691ecf188
AlexanderMcNicholl <alexander.mcnicholl@hotmail.com>
2018-03-11 14:28:00 +1000

    Begin experimental buffering, added fetch method

--- PHPCommandBridge/Scripts/main.js
+++ PHPCommandBridge/Scripts/main.js
@@ -12,14 +12,52 @@ function executeCommand(command, outElement = null, inputs = null, exec_function
 		});
 	}
 }
+// Experimental AJAX Buffering for procedural outputs.
+function AjaxBuffer(obj) {
+	var ajax = new XMLHttpRequest();
+	ajax.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			obj.success(this.responseText);
+		} else {
+			if (obj.process != null) {
+				obj.process();
+			}
+		}
+	};
+	ajax.open(obj.type, obj.url, true);
+	ajax.send();
+}
 function AjaxCall(url, exec_function) {
+	AjaxBuffer({
+		type: 'GET',
+		url: url,
+		success: function(data) {
+			exec_function(data);
+		},
+		process: function() {
+			exec_function("<pre>Processing...</pre>");
+		},
+	});
+}
+function editElementText(element, text) {
+	document.getElementById(element).innerHTML = text;
+}
+
+/*
+* Depricated functions
+*/
+
+function xhttpCall(url, exec_function) {
 	var xhttp = new XMLHttpRequest();
-	xhttp.responseType = 'text';
-	xhttp.onload = function () {
+	xhttp.onreadystatechange = function () {
+		if (this.readyState != 4) {
+			exec_function("<pre>Processing: " + this.statusText + "</pre>");
+		} else {
 			exec_function(this.responseText);
+		}
 	}
 	xhttp.open('GET', url, true);
-	xhttp.send(); // Sends the AJAX Request.
+	xhttp.send();
 	
 }
 function fetchCall(url, exec_function) {
@@ -28,7 +66,4 @@ function fetchCall(url, exec_function) {
 			exec_function(returnText);
 		});
 	});
-}
-function editElementText(element, text) {
-	document.getElementById(element).innerHTML = text;
 }
